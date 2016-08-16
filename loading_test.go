ac16cde851a941c3dbbe1fb61d75bc47bed5f29d
copaciu <daniel.copaciu@ft.com>
2016-08-16 15:56:59 +0300

    Add unit tests for transform function

--- loading_test.go
+++ loading_test.go
@@ -12,7 +12,7 @@ func TestFetchSecurities_FirstLineIsAnEquity_FirstLineIsIgnored(t *testing.T) {
 
 	fis := fetchSecurities(strings.NewReader(securities))
 	if len(fis) != 0 {
-		t.Errorf("Expected first line to be skipped!")
+		t.Error("Expected first line to be skipped!")
 	}
 }
 
@@ -168,7 +168,7 @@ func TestDoubleMD5Hash(t *testing.T) {
 func TestTransform_OneEntryInBothMappings_TransformedFIIsCorrect(t *testing.T) {
 	rawFIs := map[string][]rawFinancialInstrument{
 		"T621V4-S-KR": []rawFinancialInstrument{
-			rawFinancialInstrument{
+			{
 				securityID:      "T621V4-S-KR",
 				orgID:           "0F03DX-E",
 				inceptionDate:   "1995-03-12",
@@ -197,4 +197,194 @@ func TestTransform_OneEntryInBothMappings_TransformedFIIsCorrect(t *testing.T) {
 	if fi.figiCode != expectedFigiCode {
 		t.Errorf("Expected FIGI [%s]. Found: [%s]", expectedFigiCode, fi.figiCode)
 	}
+}
+
+func TestTransform_TwoEntriesWithSameId_OnlyTheOneWithTerminationDateIsUsed(t *testing.T) {
+	rawFIs := map[string][]rawFinancialInstrument{
+		"T621V4-S-KR": []rawFinancialInstrument{
+			{
+				securityID:      "T621V4-S-KR",
+				orgID:           "0F03DX-T",
+				inceptionDate:   "1994-07-18",
+				terminationDate: "2005-07-31",
+			},
+			{
+				securityID:      "T621V4-S-KR",
+				orgID:           "0F03DX-E",
+				inceptionDate:   "2005-07-18",
+				terminationDate: "",
+			}}}
+	figis := map[string][]string{
+		"BBG007HW10F7": []string{"T621V4-S-KR"},
+	}
+
+	expectedKey := "e81c2fed-1fe3-3b38-9e82-0f64d3074281"
+	expectedOrgID := "5a9c7643-31e4-3bad-b6ba-a7676f43da9f"
+
+	fis := transform(rawFIs, figis)
+	fi, present := fis[expectedKey]
+
+	if !present {
+		t.Errorf("Expected one financial instrument for this security id. Found: [%d]", len(fis))
+	}
+
+	if fi.orgID != expectedOrgID {
+		t.Errorf("Expecting financial instrument with orgID: [%s]. Found [%s]", expectedOrgID, fi.orgID)
+	}
+}
+
+func TestTransform_TwoEntriesWithSameId_OnlyTheLastIsUsed(t *testing.T) {
+	rawFIs := map[string][]rawFinancialInstrument{
+		"T621V4-S-KR": []rawFinancialInstrument{
+			{
+				securityID:      "T621V4-S-KR",
+				orgID:           "0F03DX-T",
+				inceptionDate:   "1994-07-18",
+				terminationDate: "",
+			},
+			{
+				securityID:      "T621V4-S-KR",
+				orgID:           "0F03DX-E",
+				inceptionDate:   "2005-07-18",
+				terminationDate: "",
+			}}}
+	figis := map[string][]string{
+		"BBG007HW10F7": []string{"T621V4-S-KR"},
+	}
+
+	expectedKey := "e81c2fed-1fe3-3b38-9e82-0f64d3074281"
+	expectedOrgID := "5a9c7643-31e4-3bad-b6ba-a7676f43da9f"
+
+	fis := transform(rawFIs, figis)
+	fi := fis[expectedKey]
+
+	if fi.orgID != expectedOrgID {
+		t.Errorf("Expecting financial instrument with orgID: [%s]. Found [%s]", expectedOrgID, fi.orgID)
+	}
+}
+
+func TestTransform_DifferentSecIdSameFigiAndEntityId(t *testing.T) {
+	rawFIs := map[string][]rawFinancialInstrument{
+		"LK1Q8B-S-HK": []rawFinancialInstrument{
+			{
+				securityID:      "LK1Q8B-S-HK",
+				orgID:           "0BFB5F-E",
+				inceptionDate:   "2011-01-27",
+				terminationDate: "2016-03-29",
+			},
+			{
+				securityID:      "LK1Q8B-S-HK",
+				orgID:           "0BFB5F-E",
+				inceptionDate:   "2016-03-29",
+				terminationDate: "",
+			}},
+		"S67X9X-S-HK": []rawFinancialInstrument{
+			{
+				securityID:      "S67X9X-S-HK",
+				orgID:           "0BFB5F-E",
+				inceptionDate:   "2016-04-01",
+				terminationDate: "",
+			}}}
+
+	figis := map[string][]string{
+		"BBG001D9T252": []string{"LK1Q8B-S-HK", "S67X9X-S-HK"},
+	}
+
+	expectedIds := []string{
+		"a135ff6a-a466-32ba-a48c-ea280cf9df6f",
+		"6372c1b4-0dee-3d99-b9a0-bef34d97d6c8",
+	}
+
+	fis := transform(rawFIs, figis)
+
+	if len(fis) != 2 {
+		t.Errorf("Expected two financial instruments but found [%d]", len(fis))
+	}
+
+	for _, expectedId := range expectedIds {
+		_, present := fis[expectedId]
+		if !present {
+			t.Errorf("Expected to found financial instrument with uuid: [%s]", expectedId)
+		}
+	}
+}
+
+func TestTransform_SameFigiSameSecIDSameEntityNoTerminationDate(t *testing.T) {
+	rawFIs := map[string][]rawFinancialInstrument{
+		"VCLG2C-S-IN": []rawFinancialInstrument{
+			{
+				securityID:      "VCLG2C-S-IN",
+				orgID:           "05VD3Y-E",
+				inceptionDate:   "2005-07-31",
+				terminationDate: "",
+			},
+			{
+				securityID:      "VCLG2C-S-IN",
+				orgID:           "05VD3Y-E",
+				inceptionDate:   "1994-07-18",
+				terminationDate: "2005-07-31",
+			},
+			{
+				securityID:      "VCLG2C-S-IN",
+				orgID:           "05VD3Y-E",
+				inceptionDate:   "2016-05-06",
+				terminationDate: "",
+			}}}
+
+	figis := map[string][]string{
+		"BBG000CXGND1": []string{"VCLG2C-S-IN"},
+	}
+
+	expectedId := "03a708f3-88ae-3c8a-a297-efe9fa0ad666"
+
+	fis := transform(rawFIs, figis)
+
+	if len(fis) != 1 {
+		t.Errorf("Expecting one financial instrument but found [%d]", len(fis))
+	}
+
+	if _, present := fis[expectedId]; !present {
+		t.Errorf("Expected to found financial instrument with uuid: [%s]", expectedId)
+	}
+}
+
+func TestTransform_SameFigiDifferentSecIDAndOrgIDNoTerminationDate(t *testing.T) {
+	rawFIs := map[string][]rawFinancialInstrument{
+		"B2GXQ6-S-CL": []rawFinancialInstrument{
+			{
+				securityID:      "B2GXQ6-S-CL",
+				orgID:           "05W5BY-E",
+				inceptionDate:   "2001-11-14",
+				terminationDate: "",
+			}},
+		"P7RDT8-S-CA": []rawFinancialInstrument{
+			{
+				securityID:      "P7RDT8-S-CA",
+				orgID:           "003XN9-E",
+				inceptionDate:   "2014-12-01",
+				terminationDate: "",
+			}}}
+
+	figis := map[string][]string{
+		"BBG000BFPHG1": []string{"B2GXQ6-S-CL", "P7RDT8-S-CA"},
+	}
+
+	expectedIds := []string{
+		"24d7f133-d30b-394f-970c-5a5e3ed66061",
+		"f9ca6c8d-1b02-3492-ae76-5ef2231e6ae7",
+	}
+
+	fis := transform(rawFIs, figis)
+
+	if len(fis) != 2 {
+		t.Errorf("Expecting two financial instruments but found [%d]", len(fis))
+	}
+
+	for _, expectedId := range expectedIds {
+		_, present := fis[expectedId]
+		if !present {
+			t.Errorf("Expected to found financial instrument with uuid: [%s]", expectedId)
+		}
+	}
+
 }
